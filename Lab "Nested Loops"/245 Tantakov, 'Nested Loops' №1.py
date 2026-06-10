b1949ce42662019d2e590a4e2efc295c558e574e
addicted <artem.tantakov@gmail.com>
2026-06-10 15:24:27 +0300

    Update 245 Tantakov, 'Nested Loops' №1.py

--- Lab "Nested Loops"/245 Tantakov, 'Nested Loops' №1.py
+++ Lab "Nested Loops"/245 Tantakov, 'Nested Loops' №1.py
@@ -1,7 +1,10 @@
+print("Задание №1 \n"
+"\"Шахматная доска\" \n")
+
 for size in range(8):
     for sizev2 in range(8):
-        if (size + sizev2) % 2 == 2:
-            print("B", end=" ")
-        
-        else:
+        if (size + sizev2) % 2 == 0:
             print("W", end=" ")
+        else:
+            print("B", end=" ")
+    print()
